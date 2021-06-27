46abf3c1443057dec5758c0609f56dc74ffb7480
ashur-k <ashurkanwal@yahoo.com>
2021-06-27 12:46:23 +0100

    setup Django restframework with postgres database

---
+++ pizza/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Pizza(models.Model):
+  name = models.CharField(max_length=200)
+  slug = models.SlugField()
+  description = models.CharField(max_length=500)
+  toppings = ArrayField(
+    models.CharField(max_length=200)
+    )
+  image = models.URLField()
+  price = models.FloatField()
+  
+  class Meta:
+    verbose_name_plural = 'Pizzas'
+    ordering = ['name', ]
+  
+  def __str__(self):
+    return self.name
+
